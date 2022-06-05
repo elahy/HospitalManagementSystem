4db0ae03f9b16a2c15931de5f1da404c72a60ac7
elahy <elahy777@gmail.com>
2022-06-05 18:19:16 +0600

    Day-4 End Session

--- models/appointment.py
+++ models/appointment.py
@@ -12,8 +12,30 @@ class HospitalAppointment(models.Model):
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
     ref = fields.Char(string='Reference', required=True, default="Self")
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High'),
+    ], string="Priority")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Canceled'),
+    ], string="Status", default='draft', required=True)
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
 
+    def action_test(self):
+        print("Hello World, Button Clicked")
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Object Button Working Perfectly!',
+                'type': 'rainbow_man',
+            }
+        }
