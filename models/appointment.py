ca721bf788a227e677ac38319df0af0bf1c48622
elahy <elahy777@gmail.com>
2022-06-05 14:26:24 +0600

    day-4 part-1

--- models/appointment.py
+++ models/appointment.py
@@ -5,8 +5,15 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
+    _rec_name = 'ref'
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
-
+    gender = fields.Selection([('male', 'Male'),('female', 'Female')], string='Gender', related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference', required=True, default="Self")
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
+
--- models/patient.py
+++ models/patient.py
@@ -1,3 +1,4 @@
+from datetime import date
 from odoo import api, fields, models
 
 
@@ -7,11 +8,18 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char(string='Name', tracking=True, required=True)
+    date_of_birth = fields.Date(string='Date Of Birth', tracking=True, required=True)
     ref = fields.Char(string='Reference', required=True, default="Self")
-    age = fields.Integer(string='Age', tracking=True, required=True)
-    gender = fields.Selection([
-        ('male', 'Male'),
-        ('female', 'Female')
-    ], required=True, tracking=True, string='Gender')
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True, required=True)
+    gender = fields.Selection([('male', 'Male'),('female', 'Female')], required=True, tracking=True, string='Gender')
     active = fields.Boolean(string="Active", default=True)
+    appointment_id = fields.Many2one(comodel_name='hospital.appointment', string="Appointments")
 
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        today = date.today()
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
