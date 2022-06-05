0ab78d6241810d605b41dfcf51ff730edfdae5dc
elahy <elahy777@gmail.com>
2022-06-05 11:04:34 +0600

    Day 3 upto 24 videos

---
+++ models/appointment.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+
+
+class HospitalAppointment(models.Model):
+    _name = "hospital.appointment"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Appointment"
+
+    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
+
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
--- models/patient.py
+++ models/patient.py
@@ -3,14 +3,15 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name', required=True)
-    ref = fields.Char(string='Reference', required=True)
-    age = fields.Integer(string='Age', required=True)
+    name = fields.Char(string='Name', tracking=True, required=True)
+    ref = fields.Char(string='Reference', required=True, default="Self")
+    age = fields.Integer(string='Age', tracking=True, required=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female')
-    ], required=True, string='Gender')
+    ], required=True, tracking=True, string='Gender')
     active = fields.Boolean(string="Active", default=True)
 
