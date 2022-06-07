f6e254e9964805b8fd4650fc2c8ee66b261b4e23
elahy <elahy777@gmail.com>
2022-06-07 15:21:39 +0600

    patient Tag Vid-54

--- __manifest__.py
+++ __manifest__.py
@@ -17,6 +17,7 @@
         'views/patient_view.xml',
         'views/female_patient_view.xml',
         'views/appointment_view.xml',
+        'views/patient_tag_view.xml',
     ],
     'demo': [],
     'installable': True,
