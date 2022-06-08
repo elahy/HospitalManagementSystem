d845fdb97955902c3f643a78ceac2bc3185e7a15
elahy <elahy777@gmail.com>
2022-06-08 11:06:04 +0600

    Start  of Day7 at Vid-61

--- wizard/cancel_appointment.py
+++ wizard/cancel_appointment.py
@@ -5,7 +5,7 @@ class CancelAppointmentWizard(models.TransientModel):
     _name = "cancel.appointment.wizard"
     _description = "Cancel Appointment Wizard"
 
-    appointment_id = fields.Many2one(string='Name')
-    active = fields.Boolean(string='Active', default=True)
-    color = fields.Integer(string='Color')
-    other_color = fields.Char(string='Other Color')
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
+    # active = fields.Boolean(string='Active', default=True)
+    # color = fields.Integer(string='Color')
+    # other_color = fields.Char(string='Other Color')
