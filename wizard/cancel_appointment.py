ecaddd66c3202133067009d6efcb4b758636acd1
elahy <elahy777@gmail.com>
2022-06-23 16:40:28 +0600

    All 162 videos

--- wizard/cancel_appointment.py
+++ wizard/cancel_appointment.py
@@ -29,6 +29,11 @@ class CancelAppointmentWizard(models.TransientModel):
             raise ValidationError(_("Sorry! Cancellation is not allowed for this booking!"))
         self.appointment_id.state = 'cancel'
         return {
-            'type': 'ir.actions.client',
-            'tag': 'reload',
+            # 'type': 'ir.actions.client',
+            # 'tag': 'reload',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'cancel.appointment.wizard',
+            'target': 'new',
+            'res_id': self.id
         }
